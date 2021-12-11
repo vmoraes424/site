4dab0b03929bbf6c9783d629702143b85d03cec8
Vinicius Moraes <vmoraes424@gmail.com>
2021-12-11 20:26:06 -0300

    Alterações no site

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { } from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Esse é meu primeiro site em React.</h1>
+      <div className="Linha"></div>
+
+      <nav className='Nav'>
+        <ul>
+          <li>Página inicial</li>
+          <li>Sobre mim</li>
+          <li>Contato</li>
+        </ul>
+      </nav>
+
+
     </div>
+
   );
 }
-
-export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, {} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Footer from './components/Footer';
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200&display=swap');
+</style>
+
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Footer email='vmoraes424@gmail.com'/>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
